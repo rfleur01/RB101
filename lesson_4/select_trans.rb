f95fd0fb874f5c9a15fb01f3ed1b514d153b5ddd
Rodshell Fleurinord <rodshellfleurinord@gmail.com>
2020-09-22 23:48:56 +0000

    pushing updates to this folder

---
+++ lesson_4/select_trans.rb
@@ -0,0 +1,13 @@
+my_numbers = [1, 4, 3, 7, 2, 6]
+
+def multiply(numbers, multiplier)
+  multiplied_numbers = []
+  counter = 0
+  
+  loop do
+    break if counter == numbers.size
+    multiplied_numbers << numbers[counter] * multiplier
+    counter += 1
+  end
+  multiplied_numbers
+end
